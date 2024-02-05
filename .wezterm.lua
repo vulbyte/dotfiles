ea6508646898c5b08bade54255b6e69a95b6a49d
insert <insert@inserts-MBP.hitronhub.home>
2024-02-04 22:44:55 -0800

    some updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,48 +1,25 @@
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local config = {}
 
---config.window_background_image = "./Users/insert/loona7.png"
-local dimmed = {
-    brightness = 0.02,
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+-- For example, changing the color scheme:
+config.color_scheme = 'Adventure'
+
+-- config.window_background_image = {
+--     source = {
+--         File = '~/.config/nvim/images/loona7.png'
+--     },
+--     width = "contain",  -- Replace with the actual width of your image
+--     height = "contain", -- Replace with the actual height of your image
+-- }
+
+config.window_background_image_hsb = {
+    brightness = 0.008,
     hue = 1.0,
     saturation = 1.0,
 }
-
-config.enable_scroll_bar = true
-
-config.background = {
-    {
-        source = {
-            File = './Users/insert/.config/nvim/images/loona7.jpg'
-        },
-        --brightness = 0.008,
-        --hue = 1.0,
-        --saturation = 1.0,
-        horizontal_align = "Left",
-        height = "Cover",
-        width = "Cover",
-
-        hsb = dimmed
-    }
-}
-
-config.font = wezterm.font('New Font')
-
---config.window_background_image_hsb = {
---  -- Darken the background image by reducing it to 1/3rd
---  brightness = 0.008,
---
---  -- You can adjust the hue by scaling its value.
---  -- a multiplier of 1.0 leaves the value unchanged.
---  hue = 1.0,
---
---  -- You can adjust the saturation also.
---  saturation = 1.0,
---
---
---  --repeat_x = 'NoRepeat',
---  height = "Cover",
---  width = "Cover",
---}
 
 return config
---
+++ .zshrc
@@ -0,0 +1,30 @@
+# bin
+export PATH="/usr/local/bin:$PATH"
+
+# rust/cargo
+#export PATH="$HOME/.cargo/bin:$PATH"
+#export PATH="$Path:/Users/insert/.cargo/bin"
+export PATH="$PATH:/opt/homebrew/bin/cargo"
+# cargo 2 electic boogaloo
+# export PATH="/Users/insert/.cargo/bin:$PATH"
+
+# mono/c#
+export PATH="/Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild:$PATH"
+export PATH="/Library/Frameworks/Mono.framework/Versions/Current/bin:$PATH"
+
+# pico stuff
+export PICO_SDK_PATH="/opt/homebrew/bin/cmake"
+
+# homebrew stuff
+export PATH="/opt/homebrew/sbin:$PATH"
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/Library/Frameworks/Python.framework/Versions/3.11/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/System/Library/Frameworks/Python:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/opt/homebrew/bin:/opt/homebrew/bin"
+
+# pnpm
+export PNPM_HOME="/Users/insert/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
