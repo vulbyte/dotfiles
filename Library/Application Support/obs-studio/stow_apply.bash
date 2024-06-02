0ff505269b3933b9bf47dd8ee099826bdbbd5022
vulbyte <vulbyt3@gmail.com>
2024-06-01 19:00:20 -0700

    updated to add obs settings

--- Library/Application Support/obs-studio/stow_apply.bash
+++ Library/Application Support/obs-studio/stow_apply.bash
@@ -1,7 +1,1 @@
-# this file is meant to clean my directory of annoying files then commit, simply to make life easier
-
-# remove all .DS_Store files
-find . -type f \( -name '.DS_Store' -or -name '*text*' \) -delete;
-
-# commit the stow
-stow .;
+../../../dotfiles/Library/Application Support/obs-studio/stow_apply.bash
