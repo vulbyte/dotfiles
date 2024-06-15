31ca28a9594aa68664c31ba372f76e27896fd2ec
vulbyte <vulbyt3@gmail.com>
2024-06-14 23:46:14 -0700

    automatic git backup made on obs shutdown

--- test.bash
+++ test.bash
@@ -1,17 +1,42 @@
-gitq_o = ''; # qgit -o (for origin)
 gitq_m = ''; # qgit -m (for message)
-alias qgit = function git_commit(){
-    while getopts 'abd:v' flag; 
-    do 
+gitq_o = ''; # qgit -o (for origin)
+gitq_y = ''; # qgit -y (for skip confirms)
+alias qgit = function git_commit(){                    
+
+
+    # check flags
+    while getopts 'abd:v' flag; do 
         case "${flag}" in 
-            gitq_o='origin';
+            m) gitq_m = "qgit @:  $datetime";;
+            o) gitq_o = 'origin';;
+            y) gitq_y = '';;
+            *) error "unexpected option ${{flag}";;
+            
+            
             if (gitq_m == ''){
                 datetime = $(date +'%Y-%m-%d %H:%M:%S')
                 gitq_m=$(echo 'qgit @: ' $datetime)
             }
             fi
+        esac
+    done
 
-    $(git add .);
-    $(git commit -m `gitq_m`);
-    $(git push -u `gitq_o`)
+    # if -y flag applied, then hard send
+    if(y == 'true'){
+        $(git add .); $(git commit -m `gitq_m`); $(git push -u `gitq_o`);
+    }
+    fi
+    else {
+        datetime = $(date +'%Y-%m-%d %H:%M:%S')
+
+        read -p "would you like to commit to: ${gitq_o} with the meddage: ${datetime} (y/n)"
+
+        select yn in "y" "n"; do
+            case $yn in
+                y) $(git add .); $(git commit -m `gitq_m`); $(git push -u `gitq_o`);;
+                n) exit;;
+                *) echo "incorrect input detected" 
+            esac
+        done
+    }
 }
