052f980c67d6c9fec0e6db426b58a154e33b660a
vulbyte <vulbyt3@gmail.com>
2024-08-26 22:55:38 -0700

    qgit @: 2024-08-26 22:55:28, from user vulbyte

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,30 +1,60 @@
 local wezterm = require 'wezterm'
+
+local window = window;
+
 local config = {}
 
---config.window_background_image = "./Users/insert/loona7.png"
 local dimmed = {
     brightness = 0.02,
     hue = 1.0,
     saturation = 1.0,
 }
 
+local height_val, width_val
+
+function recompute_padding(window)
+	local window_dims = window:get_dimensions()
+	
+	height_val = window_dims.height;
+	width_val = window_dims.width;
+-- local win_height = window.get_dimentions().pixel_height;
+-- local win_width = window.get_dimentions().pixel_width;	
+end
+
+--wezterm.on("window-resized", function(window, pane)
+--	recompute_padding(window)
+--end)
+
 config.enable_scroll_bar = true
 
 config.background = {
     {
+	source = {Color="black"},
+		width = "100%",
+		height = "100%"
+    },
+    {
         source = {
-            File = "/Users/insert/dotfiles/.config/nvim/images/loona7.png" --'/.config/nvim/images/loona7.png'
+            File = "/Users/insert/dotfiles/.config/nvim/images/pixel_mangle.gif"--'/.config/nvim/images/loona7.png'
         },
-        --brightness = 0.008,
-        --hue = 1.0,
-        --saturation = 1.0,
-        horizontal_align = "Left",
-        vertical_align = "Top",
-        height = "100%",
 
-        hsb = dimmed
-    }
+	hsb = dimmed, --{  hue = 1.0,saturation = 1.5, brightness = 1.0,} ,
+
+        horizontal_align = "Right",
+        vertical_align = "Bottom",
+	vertical_offset = "-3.5cell",
+
+        -- height = "Cover", USE THIS WITH NO WIDTH TO SET BG THAT WILL SCALE WITHOUT STRETCH
+
+	height = "428px", --(win_height*0.1),
+	width =  "536px", --(win_width*0.1),	-- auto, maybe a "aspect-ratio = 1/1;" ?
+
+	repeat_x = 'NoRepeat',
+	repeat_y = 'NoRepeat',
+    },
 }
+
+
 
 config.font = wezterm.font('New Font')
 
