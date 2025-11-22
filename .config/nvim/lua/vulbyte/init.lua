89b28da8cf24b05a29cb4fd3801a7c9174ae00c8
vulbyte <vulbyt3@gmail.com>
2025-11-21 19:34:45 -0800

    a quick backup before i remake my whole nvim config

--- .config/nvim/lua/vulbyte/init.lua
+++ .config/nvim/lua/vulbyte/init.lua
@@ -8,4 +8,4 @@ require('vulbyte.remap')
 require('vulbyte.set')
 --require('vulbyte.telescope')
 
-if (printLoadingAndLoaded == true) then print('LOADED: ./nvim/lua/vulbute/init.lua') end
+if (printLoadingAndLoaded == true) then print('LOADED: ./nvim/lua/vulbyte/init.lua') end
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/insert/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?.lua;/Users/insert/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?/init.lua;/Users/insert/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?.lua;/Users/insert/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/insert/.cache/nvim/packer_hererocks/2.1.1741730670/lib/lua/5.1/?.so"
+local package_path_str = "/Users/insert/.cache/nvim/packer_hererocks/2.1.1753364724/share/lua/5.1/?.lua;/Users/insert/.cache/nvim/packer_hererocks/2.1.1753364724/share/lua/5.1/?/init.lua;/Users/insert/.cache/nvim/packer_hererocks/2.1.1753364724/lib/luarocks/rocks-5.1/?.lua;/Users/insert/.cache/nvim/packer_hererocks/2.1.1753364724/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/insert/.cache/nvim/packer_hererocks/2.1.1753364724/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -144,11 +144,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/insert/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
-  },
-  ["nvim-treesitter"] = {
-    loaded = true,
-    path = "/Users/insert/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
-    url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["oil.nvim"] = {
     loaded = true,
